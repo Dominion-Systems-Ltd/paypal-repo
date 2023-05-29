11769e5cef9254a3edd6ffa9127c24171aae1563
Amos Egonmwan <48167887+AmosEgonmwan@users.noreply.github.com>
2023-05-29 14:13:14 -0600

    Update app.java
    Bug has been removed.

--- app.java
+++ app.java
@@ -1,8 +1,10 @@
 We support the following applications:
-java applicationss
-.net applications
-NodeJS applications
-python applications
-python applications
-web applications
-MATLAB applications
+java applicationss,
+.net applications,
+NodeJS applications,
+python applications,
+python applications,
+web applications,
+MATLAB applications,
+
+# Bug has been removed
